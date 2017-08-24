e5dcc741fc08cf76f87328e7252662ffdc7fc12a
ManisY <erqing091092@hotmail.com>
2017-08-24 17:44:30 +0800

    Create get_html_text.py

---
+++ get_html_text.py
@@ -0,0 +1,9 @@
+# get html text from url
+def getHTMLText(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
